5ff2162e83eb42df4cba415fda9e7766336dc1ea
Pranav Neyveli <prachess@gmail.com>
2018-03-19 16:14:06 -0400

    Initial Commit

---
+++ One To Fifty/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  One To Fifty
+//
+//  Created by Pranav Neyveli on 08/06/2017.
+//  Copyright © 2016 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return 50
+        
+    }
+    
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String(indexPath.row + 1)
+        
+        return cell
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
